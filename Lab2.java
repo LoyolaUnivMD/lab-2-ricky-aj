10cda90c6e2e3889d39d8c63d6503c79c6ee115f
ajthegreat63 <ajpiper@loyola.edu>
2024-01-31 17:10:36 -0500

    Quality of Life updates to Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -13,27 +13,34 @@ import java.text.DecimalFormat;
 
 class Lab2 {
     public static void main(String[] args) {
-        long prize = 225938745L;
+        long prize = 225938745L; // Initialize prize variable as 225,938,745
+
+        // Create objects
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         DecimalFormat frmt = new DecimalFormat("00");
         DecimalFormat frmt2 = new DecimalFormat("$###,###,###");
 
+        // Introduce program and ask user for name, save as customerName
         System.out.println("CS 212 - Lab 2");
         System.out.println("This program generates 10 lottery tickets.");
-        
         System.out.print("What's your name? ");
         String customerName = input.next();
 
+        // Generate 10 tickets with 6 numbers each
         for (int i = 0; i <= 10; i++){
             for (int j = 0; j <= 6; j++){
                 System.out.print(frmt.format(rand.nextInt(100)) + " ");
             }
-            System.out.println();
+            System.out.println(); // Separate lines of tickets
+
         }
+
+        // Generate good luck message
         System.out.println("_________________________________________");
         System.out.println("Good Luck, " + customerName);
-        System.out.println("Your estimated winnings are " + frmt2.format(prize));
+        System.out.println("Your estimated jackpot is:");
+        System.out.println(frmt2.format(prize));
         System.out.println("_________________________________________");
 
 
